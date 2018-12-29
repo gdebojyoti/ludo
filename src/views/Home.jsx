29282cf848e34eea53dac269cb7f0c288488ec06
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2018-12-29 20:16:04 +0530

    Basic grid

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,52 @@ import React, { Fragment, PureComponent } from 'react'
 import '../styles/views/home'
 
 const HomeView = () => {
-  return <h1>Ludo - a PWA.</h1>
+  return <div>
+    <h1>Ludo - a PWA.</h1>
+    <Grid />
+  </div>
+}
+
+const Grid = () => {
+  return <div className='grid'>
+    <Quadrant className='quadrant--red' />
+    <Quadrant className='quadrant--blue' />
+    <Quadrant className='quadrant--yellow' />
+    <Quadrant className='quadrant--green' />
+
+    <Center />
+  </div>
+}
+
+const Quadrant = props => {
+  const { className } = props
+  const divClass = 'quadrant' + (className ? ' ' + className : '')
+  return <div className={divClass}>
+    <div className='quadrant__home'></div>
+    <div className='quadrant__rows'>
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  </div>
+}
+
+const Row = () => {
+  const cells = []
+  for (let i = 0; i < 6; i++) {
+    cells.push(<Cell key={i} />)
+  }
+  return <div className='quadrant__row'>
+    { cells }
+  </div>
+}
+
+const Cell = () => {
+  return <div className='quadrant__cell'></div>
+}
+
+const Center = () => {
+  return <div className='grid__center'></div>
 }
 
 export default HomeView
